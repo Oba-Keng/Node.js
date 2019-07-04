3011ae61433d9a97378b51309e24e0c4054aa8ab
Obakeng <obeezus@gmail.com>
2019-07-04 18:00:23 +0200

    second commit

--- Task4:api/routes/api.js
+++ Task4:api/routes/api.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const schema = require("../handler/prospecthandler");
+const ActionHandler = require("../handler/prospecthandler");
 
-//initialize routes
-router.use(schema);
+var phandler = new ActionHandler();
 
+router.get("/", (req, res) => {
+  res.send("welcome");
+});
+
+router.get("/users", (req, res) => {
+  phandler.viewProspects(req, res);
+});
+
+router.get("/users/:id", (req, res) => {
+  phandler.viewProspect(req, res);
+});
+router.post("/users", (req, res) => {
+  phandler.addProspect(req, res);
+});
+router.put("/users/:id", (req, res) => {
+  phandler.updateProspect(req, res);
+});
+router.delete("/users/:id", (req, res) => {
+  phandler.deleteProspect(req, res);
+});
+router.delete("/users", (req, res) => {
+  phandler.deleteProspects(req, res);
+});
 module.exports = router;
